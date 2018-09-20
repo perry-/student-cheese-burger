245e5c5a8f5e875ca06b940ebb2bee0e9554a6e9
AsplemEirik <eirik.asplem@bouvet.no>
2018-09-20 12:40:55 +0200

    Fixed undefined penguin

--- myFirstPenguin/index.js
+++ myFirstPenguin/index.js
@@ -51,13 +51,13 @@ function doesCellContainWall(walls, x, y) {
 function wallInFrontOfPenguin(body) {
     switch(body.you.direction) {
         case "top":
-            return doesCellContainWall(body.walls, penguin.x, penguin.y--);
+            return doesCellContainWall(body.walls, body.you.x, body.you.y--);
         case "bottom":
-            return doesCellContainWall(body.walls, penguin.x, penguin.y++);
+            return doesCellContainWall(body.walls, body.you.x, body.you.y++);
         case "left":
-            return doesCellContainWall(body.walls, penguin.x--, penguin.y);
+            return doesCellContainWall(body.walls, body.you.x--, body.you.y);
         case "right":
-            return doesCellContainWall(body.walls, penguin.x++, penguin.y);
+            return doesCellContainWall(body.walls, body.you.x++, body.you.y);
         default:
             return true;
     }
